6dbad549fea9e44a347f91bf4d1c4445500e385a
joao <joaoreis1@unipam.edu.br>
2021-05-09 01:50:33 -0300

    conexao formulário com API

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-    'baseURL': 'http://localhost:3333/'
-})
+    // 'baseURL': 'http://localhost:5000/api/'
+    'baseURL': 'http://localhost:8080/api'
+
+}) 
 
 export default api
